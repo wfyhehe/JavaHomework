ed1e4e048383e7f40683b3e2a3d28d3809cadbf7
wfyhehe <826487285@qq.com>
2018-04-24 14:46:24 +0800

    zoominout

--- src/com/wfy/work3/e8/ZoomInOut.java
+++ src/com/wfy/work3/e8/ZoomInOut.java
@@ -5,49 +5,45 @@ import java.awt.*;
 
 public class ZoomInOut extends Applet {
     public static final String text = "Hello WFY";
-    private Label textLabel;
     private Button in, out;
     private int fontSize = 28;
 
     public void init() {
         setLayout(null);
-        textLabel = new Label();
-        textLabel.setBounds(20, 20, 400, 200);
-        textLabel.setFont(new Font(text, Font.BOLD, fontSize));
-        add(textLabel);
-        textLabel.setText(text);
 
         in = new Button("+");
-        in.setBounds(110, 230, 40, 40);
+        in.setBounds(110, 200, 40, 40);
         add(in);
 
         out = new Button("-");
-        out.setBounds(155, 230, 40, 40);
+        out.setBounds(155, 200, 40, 40);
         add(out);
     }
 
+    @Override
     public boolean action(Event e, Object o) {
         if ((e.target == in)) {
             zoomIn();
         }
-
         if (e.target == out) {
             zoomOut();
         }
-
         return true;
     }
 
-
     private void zoomIn() {
         fontSize += 1;
-        textLabel.setFont(new Font(text, Font.BOLD, fontSize));
         repaint();
     }
 
     private void zoomOut() {
         fontSize -= 1;
-        textLabel.setFont(new Font(text, Font.BOLD, fontSize));
         repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setFont(new Font(text, Font.BOLD, fontSize));
+        g.drawString(text, 25, 100);
     }
 }
